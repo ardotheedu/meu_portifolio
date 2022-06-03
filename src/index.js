46850d6ae08154bf9e3677558bee425e9da0419a
ardotheedu <eduardosalima3@gmail.com>
2022-06-02 23:14:44 -0300

    feat: header routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.scss';
 import App from './pages/App';
 import Homepage from './routes/Homepage';
 import Portifolio from './routes/Portifolio';
+import Servicos from './routes/Servicos';
+import Sobre from './routes/Sobre';
+import Contato from './routes/Contato';
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 ReactDOM.render(
@@ -12,6 +15,9 @@ ReactDOM.render(
       <Route path="/" element={<App />} />
       <Route path="homepage" element={<Homepage />} />
       <Route path="portifolio" element={<Portifolio />} />
+      <Route path="servicos" element={<Servicos />} />
+      <Route path="sobre" element={<Sobre />} />
+      <Route path="contato" element={<Contato />} />
     </Routes>
   </Router>,
   document.getElementById('root')
